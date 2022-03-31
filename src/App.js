4ba5acc6f9708d0020eed802b4862de5b79c062d
dudanny712 <58696798+dudanny712@users.noreply.github.com>
2022-03-31 23:19:03 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -9,14 +9,12 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout/>}>
-      <Route index element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      </Route>
-    </Routes>
-    
-    
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+     </Routes>
     </>
   );
 }
